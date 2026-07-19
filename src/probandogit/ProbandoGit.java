6b9d0e3ba7c5c983748d95eb6f3fb9d6677e8343
Fercho <fernandoaltamirano.vdc@gmail.com>
2026-07-19 09:34:07 -0600

    modifying newClass objects identifier

--- src/probandogit/ProbandoGit.java
+++ src/probandogit/ProbandoGit.java
@@ -16,7 +16,7 @@ public class ProbandoGit {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Yes");
-        NewClass newClass = new NewClass();
+        NewClass newCla = new NewClass();
         
         
     }
